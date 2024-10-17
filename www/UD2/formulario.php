cf90bc9f15d5d716b2dd4ae976f4436568ea2202
joserosalessueiro <jrsueiro@yahoo.es>
2024-10-17 22:58:35 +0200

    Avanzando en estructura del formulario

--- www/UD2/formulario.php
+++ www/UD2/formulario.php
@@ -16,78 +16,26 @@
     <main>
         <form action="#" method="post">
             <fieldset>
-                <legend>Datos Personales</legend>
-                <label for="nombre">Nombre:</label>
-                <input type="text" id="nombre" name="nombre" required><br>
-
-                <label for="apellidos">Apellidos:</label>
-                <input type="text" id="apellidos" name="apellidos" required><br>
-
-                <label for="fecha-nacimiento">Fecha de Nacimiento:</label>
-                <input type="date" id="fecha-nacimiento" name="fecha-nacimiento" required><br>
-
-                <label for="dni">DNI:</label>
-                <input type="text" id="dni" name="dni" required><br>
+                <legend>Datos Agenda</legend>
+               
             </fieldset>
 
             <fieldset>
-                <legend>Datos de Contacto</legend>
+                <legend>Nuevo Contacto</legend>
+                <label for="name">Nombre:</label>
+                <input type="name" id="name" name="name" required><br>
                 <label for="telefono">Teléfono:</label>
                 <input type="tel" id="telefono" name="telefono" required><br>
 
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required><br>
+                <button type="submit">Añadir Contacto</button>
+                <button type="submit">Limpiar Campos</button>
             </fieldset>
 
             <fieldset>
-                <legend>Información Académica</legend>
-                <label for="titulacion">Titulación de Acceso:</label>
-                <input type="text" id="titulacion" name="titulacion" required><br>
-
-                <label for="nota-acceso">Nota de Acceso:</label>
-                <input type="number" id="nota-acceso" name="nota-acceso" min="0" max="10" step="0.1" required><br>
+                <legend>Vaciar Agenda</legend>
+                <button type="submit">Vaciar</button>
             </fieldset>
 
-            <fieldset>
-                <legend>Residencia</legend>
-                <label for="provincia">Provincia:</label>
-                <input type="text" id="provincia" name="provincia" required><br>
-
-                <label for="codigo-postal">Código Postal:</label>
-                <input type="text" id="codigo-postal" name="codigo-postal" required><br>
-
-                <label for="alquiler">Alquiler/Residencia:</label>
-                <input type="text" id="alquiler" name="alquiler" required><br>
-
-                <label for="direccion-alquiler">Dirección de Alquiler:</label>
-                <input type="text" id="direccion-alquiler" name="direccion-alquiler"><br>
-
-                <label for="precio-alquiler">Precio Alquiler:</label>
-                <input type="number" id="precio-alquiler" name="precio-alquiler" step="0.01"><br>
-            </fieldset>
-
-            <fieldset>
-                <legend>Beca</legend>
-                <label for="beca">¿Solicita Beca? (S/N):</label>
-                <select id="beca" name="beca" required>
-                    <option value="si">Sí</option>
-                    <option value="no">No</option>
-                </select><br>
-
-                <label for="nota-acceso">Nota de Acceso:</label>
-                <input type="number" id="nota-acceso" name="nota-acceso" min="0" max="10" step="0.1" required><br>
-            </fieldset>
-
-            <fieldset>
-                <legend>Datos Familiares</legend>
-                <label for="nombre-padre">Nombre del Padre:</label>
-                <input type="text" id="nombre-padre" name="nombre-padre"><br>
-
-                <label for="nombre-madre">Nombre de la Madre:</label>
-                <input type="text" id="nombre-madre" name="nombre-madre"><br>
-            </fieldset>
-
-            <button type="submit">Enviar</button>
         </form>
     </main>
 
